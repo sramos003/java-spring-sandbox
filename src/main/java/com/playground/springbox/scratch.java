7e1ab28be11f94c0f39399659f25c9e6e8bd9c08
Steve A Ramos <52667362+sramos003@users.noreply.github.com>
2023-02-02 19:04:36 -0500

    Update scratch.java

--- src/main/java/com/playground/springbox/scratch.java
+++ src/main/java/com/playground/springbox/scratch.java
@@ -1,20 +1,26 @@
 import java.text.DecimalFormat;
+import java.util.List;
 
 class Scratch {
+
     static class DigitCounter {
-        public static void evaluateNumber(Object numToTest, int numberEquals) {
+        enum CounterTypes {
+            DecimalCounter, DigitCounter
+        }
+        public static void evaluateNumber(Object numToTest, int numberEquals, CounterTypes counterTypes) {
             // Return decimal formatted number initialized with starting/ending 0s
             DecimalFormat decimalFormat = new DecimalFormat("00000.00000");
             String messageToDisplay;
             String numberToPrint;
+            String subMessage = CounterTypes.DigitCounter.equals(counterTypes) ? "digit" : "decimal";
             if (numToTest instanceof Double || numToTest instanceof Float) {
-                messageToDisplay = (numToTest instanceof Double ? "double" : "float") + " number test ";
-                numberToPrint = decimalFormat.format(numberEquals);
+                messageToDisplay = String.format("%s %s number test", (numToTest instanceof Double ? "double" : "float"), subMessage);
+                numberToPrint = decimalFormat.format(numToTest);
             } else if (numToTest instanceof String) {
-                messageToDisplay = "string number test ";
+                messageToDisplay = String.format("string %s number test ", subMessage);
                 numberToPrint = decimalFormat.format(Double.parseDouble((String) numToTest));
             } else {
-                messageToDisplay = "integer number test ";
+                messageToDisplay = String.format("integer %s number test ", subMessage);
                 numberToPrint = String.valueOf(numToTest);
             }
             var index = numberToPrint.indexOf(".");
@@ -26,23 +32,45 @@ class Scratch {
         }
     }
 
+    static class Digit {
+        private final Object numberToTest;
+        private final int numberToEvaluate;
+
+        public Digit(Object numberToTest, int numberToEvaluate) {
+            this.numberToTest = numberToTest;
+            this.numberToEvaluate = numberToEvaluate;
+        }
+
+        public void evaluateNumber(DigitCounter.CounterTypes counterTypes) {
+            DigitCounter.evaluateNumber(this.numberToTest, this.numberToEvaluate, counterTypes);
+        }
+    }
+
+    static class DigitWrapper {
+        List<Digit> digitList;
+        DigitCounter.CounterTypes counterTypes;
+
+        public DigitWrapper(List<Digit> digitList, DigitCounter.CounterTypes counterTypes) {
+            this.digitList = digitList;
+            this.counterTypes = counterTypes;
+        }
+
+        public void evaluateNumber() {
+            digitList.forEach((digitItem) -> digitItem.evaluateNumber(counterTypes));
+        }
+    }
+
     public static void main(String[] args) {
-        double digitDouble = 001234.22000d;
-        double decimalDouble = 123.4500d;
-        float digitFloat = 123.45f;
-        float decimalFloat = 123.45f;
+        int three = 3, five = 5;
+        double digitDouble = 001234.22000d, decimalDouble = 123.4500d;
+        float digitFloat = 123.45f, decimalFloat = 123.45f;
         int number = 123;
         String string = "123.45000";
-        // ---
-        int three = 3;
-        int five = 5;
-        DigitCounter.evaluateNumber(digitDouble, five);
-        DigitCounter.evaluateNumber(digitFloat, five);
-        DigitCounter.evaluateNumber(number, three);
-        DigitCounter.evaluateNumber(string, five);
-        DigitCounter.evaluateNumber(decimalDouble, five);
-        DigitCounter.evaluateNumber(decimalFloat, five);
-        DigitCounter.evaluateNumber(number, three);
-        DigitCounter.evaluateNumber(string, five);
+        // -- Lists of data
+        List<Digit> digitList = List.of(new Digit(digitDouble, five), new Digit(digitFloat, five), new Digit(number, three), new Digit(string, five));
+        List<Digit> decimalList = List.of(new Digit(decimalDouble, five), new Digit(decimalFloat, five), new Digit(number, three), new Digit(string, five));
+        // -- Run logic
+        new DigitWrapper(digitList, DigitCounter.CounterTypes.DigitCounter).evaluateNumber();
+        new DigitWrapper(decimalList, DigitCounter.CounterTypes.DecimalCounter).evaluateNumber();
     }
 }
